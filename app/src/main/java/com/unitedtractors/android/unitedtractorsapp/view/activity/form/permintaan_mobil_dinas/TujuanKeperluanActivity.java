c303f3b949e9f214431c02ee0133fb5a735cce70
Rey <firnasreyhan@gmail.com>
2021-02-18 16:23:02 +0700

    Fix directory

--- app/src/main/java/com/unitedtractors/android/unitedtractorsapp/view/activity/form/permintaan_mobil_dinas/TujuanKeperluanActivity.java
+++ app/src/main/java/com/unitedtractors/android/unitedtractorsapp/view/activity/form/permintaan_mobil_dinas/TujuanKeperluanActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.unitedtractors.android.unitedtractorsapp.R;
+
 public class TujuanKeperluanActivity extends AppCompatActivity {
 
     @Override
